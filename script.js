23956c5dcb5c6ee6f8917ee721fa51cb23620038
Precious Dafitohwo <preciousmaro@rocketmail.com>
2024-03-22 08:44:13 +0000

    modifiied js file

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ const h1 = document.querySelector("h1");
 console.log(h1);
 
 h1.textContent = "Does this really work?";
-const image = document.querySelector("img");
+const image = document.createElement("img");
 image.src ="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?cs=srgb&dl=daylight-environment-forest-459225.jpg&fm=jpg";
 image.alt = "stunning views";
 body.appendChild(image);
-const h3 = document.querySelector("h3");
 const h3 = document.createElement("h3");
+
 h3.textContent = "I really hope this works";
+body.appendChild(h3);
